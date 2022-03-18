f60956ff55b506a7229e2cedee3b8c97c0563b72
kubealex <al.rossi87@gmail.com>
2022-03-18 14:32:02 +0100

    Increasing resources for satellite

--- terraform/modules/01_satellite_instance/variables.tf
+++ terraform/modules/01_satellite_instance/variables.tf
@@ -13,13 +13,13 @@ variable domain {
 variable memory {
   type        = number
   description = "Amount of memory in GBs for the VM"
-  default     = 8
+  default     = 32
 }
 
 variable cpu {
   type        = number
   description = "Amount of CPUs for the VM"
-  default     = 2
+  default     = 4
 }
 
 variable libvirt_network {
--- terraform/resetks.sh
+++
@@ -1,3 +0,0 @@
-mkfs.ext2 -d kickstart/rhel7-satellite -L OEMDRV modules/01_satellite_instance/oemdrv.img
-mkfs.ext2 -d kickstart/rhel7 -L OEMDRV modules/02_client_instance/oemdrv-rhel7.img
-mkfs.ext2 -d kickstart/rhel8 -L OEMDRV modules/02_client_instance/oemdrv-rhel8.img
